a60664202ed1f820867fb5457fd7672b37fdd861
Cho-Ik-Jun <harry960629@naver.com>
2023-10-25 21:29:49 +0900

    ✨ feat: 기존 파일 삭제 후 노션 클로닝 프로젝트 업로드

---
+++ src/makeDocTree.js
@@ -0,0 +1,34 @@
+import { addStorage, getStorage } from './storage.js';
+
+export function makeDocTree(root, depth, domTree = []) {
+  // 루트 문서 순회
+  const toggledList = getStorage('toggled', []);
+
+  root.forEach((child) => {
+    if (toggledList.indexOf(child.id.toString()) === -1) {
+      toggledList.push(child.id.toString());
+      addStorage('toggled', toggledList);
+    }
+
+    const dom = `
+      <div data-depth="${depth}" data-id="${
+      child.id
+    }"class="nav-document-container" style="padding-left: ${
+      5 + (depth - 1) * 10
+    }px">
+        <button data-id="${child.id}" class="nav-toggle-btn toggled">▼</button>
+        <div class="nav-document" data-id="${child.id}">${child.title}</div>
+        <button data-id="${child.id}" class="nav-delete-btn hidden">✖</button>
+        <button data-id="${child.id}" class="nav-plus-btn hidden">➕</button>
+      </div>
+      `;
+
+    domTree.push(dom);
+
+    if (child.documents.length === 0) {
+      return domTree;
+    }
+    // 하위 문서 뎁스 추가 후 탐색
+    makeDocTree(child.documents, depth + 1, domTree);
+  });
+}
